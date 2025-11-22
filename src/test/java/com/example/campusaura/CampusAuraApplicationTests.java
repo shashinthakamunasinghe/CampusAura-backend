f5e11c6097a5417fbf53b6219607ad0c5d7ea163
Sandalu Rubasinghe <sandalurubasinghe@gmail.com>
2025-11-22 16:11:09 +0530

    adding file structure of CampusAura

---
+++ src/test/java/com/example/campusaura/CampusAuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusAuraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

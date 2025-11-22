8255004a3b4a4c651d4c7682da1ca6edf87762ea
sandaluruba <sandalurubasinghe@gmail.com>
2025-11-22 17:28:43 +0530

    Adding file structure to the backend

---
+++ src/main/java/com/example/campusaura/CampusAuraApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusAuraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusAuraApplication.class, args);
+    }
+
+}
